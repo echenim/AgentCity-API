f8fc05c9c39c7e7164a921a17da9ba1e7053146e
Myron William Echenim <myron.echenim@live.com>
2019-04-19 20:24:28 -0700

    functions for manipulating string data

---
+++ src/agentcity/utilities/string_util.clj
@@ -0,0 +1,25 @@
+(ns agentcity.utilities.string_util
+  (:require [clojure.string :as str]))
+
+(def non-blank? (complement str/blank?))
+
+(def max-lenght [length text]
+  (<= (count text) length))
+
+(defn max-length? [length text]
+  (<= (count text) length))
+
+(defn non-blank-with-max-length? [length text]
+  (and (non-blank? text) (max-length? length text)))
+
+(defn min-length? [length text]
+  (>= (count text) length))
+
+(defn length-in-range? [min-length max-length text]
+  (and (min-length? min-length text) (max-length? max-length text)))
+
+(def email-regex
+  #"(?i)[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+
+(defn email? [email]
+  (boolean (and (string? email) (re-matches email-regex email))))  
